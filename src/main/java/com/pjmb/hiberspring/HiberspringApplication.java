60e0c0eba8553955e4077ed492bed499c58b9f38
PJMB <151967860+pjmbusnel@users.noreply.github.com>
2023-12-07 14:26:10 -0500

    add files from spring initializr

---
+++ src/main/java/com/pjmb/hiberspring/HiberspringApplication.java
@@ -0,0 +1,13 @@
+package com.pjmb.hiberspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjmb/hiberspring/HiberspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjmb.hiberspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
